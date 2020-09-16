3d25cbae03344166a5ec42d9a2e24695639c92f3
unknown <waltershilman@gmail.com>
2020-09-16 14:46:49 -0300

    delete income

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -1,24 +1,7 @@
 const Income = require("../models/Income");
 const { validationResult } = require("express-validator");
 
-exports.createIncome = async (req, res) => {
-  try {
-    const errores = validationResult(req);
-    if (!errores.isEmpty()) {
-      return res.status(400).json({ errores: errores.array() });
-    }
-    // crea el nuevo usuario
-    console.log(req.body);
-    const income = new Income(req.body);
-
-    await income.save();
-    res.json({ income });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ msg: "Hubo un error al Crear el Ingreso" });
-  }
-};
-
+// get ingresos 
 exports.getIncomes = async (req, res) => {
   try {
     const { email } = req.params;
@@ -38,3 +21,42 @@ exports.getIncomes = async (req, res) => {
       .json({ msg: "Hubo un error al Obtener los Ingresos" });
   }
 };
+
+// alta de ingreso
+exports.createIncome = async (req, res) => {
+  try {
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+      return res.status(400).json({ errores: errores.array() });
+    }
+    // crea el nuevo ingreso
+    console.log(req.body);
+    const income = new Income(req.body);
+
+    await income.save();
+    res.json({ income });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ msg: "Hubo un error al Crear el Ingreso" });
+  }
+};
+
+// baja de ingreso
+exports.deleteIncome = async (req, res) => {
+  try {
+    let income = await Income.findById(req.params.id);
+
+    if (!income) {
+      return res.status(404).json({ msg: "No existe el ingreso" });
+    }
+
+    await Income.findOneAndRemove({ _id: req.params.id });
+    res.json({ msg: "Ingreso Eliminado" });
+} catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
+
+// modificacion de ingreso
+
